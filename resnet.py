895b1ae05a7cba2e68ae0ce5b6bbc87b5ea57fa8
andytungsz <rainmay_service1@hotmail.com>
2022-10-19 16:07:34 +0800

    new public pat

---
+++ resnet.py
@@ -0,0 +1,71 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from resnet import ResNet18
+
+
+class ResBlock(nn.Module):
+    def __init__(self, in_channel, out_channel, stride=1):
+        super(ResBlock, self).__init__()
+        self.left = nn.Sequential(
+                nn.Conv2d(in_channel, out_channel, kernel_size=3, stride=stride, padding=1, bias=False),
+                nn.BatchNorm2d(out_channel),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(out_channel, out_channel, kernel_size=3, stride=1, padding=1, bias=False),
+                nn.BatchNorm2d(out_channel)
+        )
+        self.shortcut = nn.Sequential()
+
+        if stride != 1 or in_channel != out_channel:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channel, out_channel, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channel)
+            )
+
+    def forward(self, x):
+        out = self.left(x)
+        out = out + self.shortcut(x)
+        out = F.relu(out)
+
+        return out
+
+
+class ResNet18(nn.Module):
+    def __init__(self, ResBlock, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_channel=64
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1,padding=1, bias=False), 
+            nn.BatchNorm2d(64),
+            nn.ReLU()
+        )
+
+        self.layer1 = self.make_layer(ResBlock, 64, 2, stride=1)
+        self.layer2 = self.make_layer(ResBlock, 128, 2, stride=2)
+        self.layer3 = self.make_layer(ResBlock, 256, 2, stride=2)
+        self.layer4 = self.make_layer(ResBlock, 256, 2, stride =2)
+        self.fc = nn.Linear(512, num_classes)
+
+    def make_layer(self, block, channels, num_blocks, stride):
+        strides = [stride] = [1] * (num_blocks -1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.inchannel, channels, stride))
+            self.inchannel = channels
+        return nn.Sequential(*layers)
+    
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+
+        return out
